18571c04021a4bc308ef91160994d66c3abd8598
VigneshKarthigeyan <46390982+VigneshKarthigeyan@users.noreply.github.com>
2019-12-16 21:50:32 +0530

    Add files via upload

---
+++ 0th_day/printstate.py
@@ -0,0 +1,6 @@
+#Declare a string value
+str=''
+#Read and save the variable
+str=input()
+#Print the variable
+print(str)
---
+++ 1st_day/string_to_int&float.py
@@ -0,0 +1,19 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+sint=0
+sdou=0.0
+str=""
+# Read and save an integer, double, and String to your variables.
+sint=int(input())
+sdou=float(input())
+str=input()
+# Print the sum of both integer variables on a new line.
+print(i+sint)
+# Print the sum of the double variables on a new line.
+print(d+sdou)
+# Concatenate and print the String variables on a new line
+print(s+str)
+# The 's' variable above should be printed first.
+
