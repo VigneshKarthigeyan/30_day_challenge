fe2ca780883e94cc5b0e4592d180014bf68687b9
VigneshKarthigeyan <46390982+VigneshKarthigeyan@users.noreply.github.com>
2019-12-18 19:56:23 +0530

    Update printstate.py

--- 0th_day/printstate.py
+++ 0th_day/printstate.py
@@ -1,6 +1,8 @@
-#Declare a string value
-str=''
-#Read and save the variable
-str=input()
-#Print the variable
-print(str)
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
+print(input_string)
